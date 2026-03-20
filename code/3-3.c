c8881573f49c02c6064983b7be31e2537f6e464b
thototh <144813512+thototh@users.noreply.github.com>
2026-03-20 11:44:28 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdarg.h>
 
-int main() {
+
+int average(int num, ...){
+    va_list ap;
+    va_start(ap, num);
+    int total = 0;
+    for(int i = 0; i < num; i++){
+        total += va_arg(ap, int);
+    }
+    va_end(ap);
+    return total/num;
+}
+
+int main(void){
+    printf("%d\n", average(3, 10, 20, 30));
+    printf("%d\n", average(5, 10, 20, 30, 40, 50));
 
     return 0;
+
 }
+
+
