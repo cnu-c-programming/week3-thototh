8d8185ba35b482c917ecfea57e4d61d88df2c195
thototh <144813512+thototh@users.noreply.github.com>
2026-03-20 11:27:26 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main() {
+void swap(int *x, int*y){
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main(void){
+    int x = 100, y = 200;
+
+    printf("x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("x = %d, y = %d\n", x, y);
 
     return 0;
+
 }
+
+
