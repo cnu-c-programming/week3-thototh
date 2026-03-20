2197986cd533275902735bd4ba1cf8ed35e03009
thototh <144813512+thototh@users.noreply.github.com>
2026-03-20 11:20:36 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main() {
+int power(int base, int exp){
+    if(exp==0) return 1;
+    else if(exp==1) return base;
+    else return base * power(base, exp-1);
+}
+
+int main(void){
+    printf("%d\n", power(2, 3));
+    printf("%d\n", power(5, 0));
+    printf("%d\n", power(3, 4));
 
     return 0;
 }
+
+
