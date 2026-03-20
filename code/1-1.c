87473a1fbb3d4e6c3c96e5efccaf87486e734236
thototh <144813512+thototh@users.noreply.github.com>
2026-03-20 10:59:36 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
-int main() {
-
+int add(int a, int b){
+    return a+b;
+}
+int sub(int a, int b){
+    return a-b;
+}
+int mul(int a, int b){
+    return a*b;
+}
+int div(int a, int b){
+    return a/b;
+}
+int main(void){
+    int a = 10, b = 5;
+    printf("10 + 5 = %d\n", add(a, b));
+    printf("10 - 5 = %d\n", sub(a, b));
+    printf("10 * 5 = %d\n", mul(a, b));
+    printf("10 / 5 = %d\n", div(a, b));
+    
     return 0;
 }
