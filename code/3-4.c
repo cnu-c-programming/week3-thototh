fe78006907265bf2b6ec4880f264d759607c5df9
thototh <144813512+thototh@users.noreply.github.com>
2026-03-20 11:48:50 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int main() {
+
+int max_of(int num, ...){
+    va_list ap;
+    va_start(ap, num);
+    int front = va_arg(ap, int);
+    for(int i = 0; i < num-1; i++){
+        int next = va_arg(ap, int);
+        if(next > front) front = next;
+    }
+    va_end(ap);
+    return front;
+}
+
+int main(void){
+    printf("%d\n", max_of(4, 5, 12, 3, 9));
+    printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
+    printf("%d\n", max_of(3, 100, 200, 300));
 
     return 0;
+
 }
+
+
