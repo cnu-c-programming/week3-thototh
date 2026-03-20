dafec26d84a7c1d10aa8367df253e4711196ef35
thototh <144813512+thototh@users.noreply.github.com>
2026-03-20 11:22:29 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main() {
+void increment(int x){
+    ++x;
+    printf("%d\n", x);
+}
+
+int main(void){
+    int x = 10;
+    
+    printf("%d\n", x);
+    increment(x);
+    printf("%d\n", x);
 
     return 0;
+
 }
+
+
